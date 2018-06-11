a2f8b34be44ecb31d7bd2955371ee3013eee17ab
Team-on <timonsol13@gmail.com>
2018-06-11 20:44:07 +0300

    Тестова виводка доріг

--- source/game/IO/GameCell.cs
+++ source/game/IO/GameCell.cs
@@ -18,20 +18,41 @@ namespace TownsAndWarriors.game.map {
 	partial class GameCell {
 		public override void InitializeShape() {
 			shape = new Grid();
+			shape.Background = Brushes.Black;
 
-			var rect = new Rectangle();
-			if (IsOpenLeft && IsOpenBottom)
-            {
-                rect.Fill = Brushes.Aqua;
+			Rectangle rect;
+			if (IsOpenTop) {
+				rect = FormRect();
+				rect.Height = 30;
+				rect.VerticalAlignment = VerticalAlignment.Top;
+				shape.Children.Add(rect);
+			}
+			if (IsOpenBottom) {
+				rect = FormRect();
+				rect.Height = 30;
+				rect.VerticalAlignment = VerticalAlignment.Bottom;
+				shape.Children.Add(rect);
+			}
+			if (IsOpenLeft) {
+				rect = FormRect();
+				rect.Width = 30;
+				rect.HorizontalAlignment = HorizontalAlignment.Left;
+				shape.Children.Add(rect);
+			}
+			if (IsOpenRight) {
+				rect = FormRect();
+				rect.Width = 30;
+				rect.HorizontalAlignment = HorizontalAlignment.Right;
+				shape.Children.Add(rect);
+			}
 
-            }
-            else if(IsOpenLeft || IsOpenRight)
-				rect.Fill = Brushes.Red;
-			else if (IsOpenTop || IsOpenBottom)
-				rect.Fill = Brushes.Blue;
-			else
-				rect.Fill = Brushes.Black;
-			shape.Children.Add(rect);
+			Rectangle FormRect() {
+				return new Rectangle() {
+					Fill = Brushes.LightGray,
+					Width = 15,
+					Height = 15
+			};
+			}
 		}
 	}
 }
